6e6365ba39a78d3843347cb5796ea27663d8cad2
Christina Gordon <crissyg@users.noreply.github.com>
2024-12-14 03:30:18 -0500

    Updated README and logIN-Bot.py

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Auto-Login bot
- Auto-login bot using Python Selenium library
+ Auto-login bot using Python Selenium library.
+ Browser used: Chrome
 
 ## Pre-reqs:
 1.  Install selenium using the below command:
@@ -13,12 +14,23 @@
     - Paste the URL in your browser and download
     - Unzip file and oberve that a chromedriver executable is in the directory
     - Copy the chromedriver executable and paste it in your usr/local/bin directory
-    - On Mac, Got to folder /usr/local/bin and Paste the chromedriver there.
+    - On Mac or Linux, Got to folder /usr/local/bin and Paste the chromedriver there.
     - Double click to chromedriver to test. If you pop-up window stating that it's blocked, go to privacy and security settings on MacOS, scroll to security section and you should see chromedriver was blocked and click Run anyway. Then run chromedriver again.
 
 ## How to Run Bot:
 
 1. Copy and save logIn-Bot.py locally on your PC.
-2. Edit line 37 with the URL path your want the bot to Auto-login.
-3. Edit lines 32 and 33 with your usersame and password for URL in step 2.
+2. Edit line 45 with the URL you want the bot to Auto-login.
+3. Edit lines 40 and 41 with your usersame and password for URL in step 2.
+4. Run logIn-Bot.py in the terminal.
+'''
+python3 logIn-Bot.py 
+'''
 
+## How to find ID element and NAME fields in Chrome
+1. Open Chrome and navigate to log-in page of your choice.
+2. Right-click on the field or button and select <strong>inspect.</strong>
+3. The <strong>Element</strong> tab will open up on the right-hand side of the page in the Dev mode.
+4. The seleced field  or button section will be hightlight.
+5. Make note of the ID or NAME title. The snippet below is an exmaple of the ID title for the username field:
+![Username field and element ID name](images/image.png)
--- logIN-Bot.py
+++ logIN-Bot.py
@@ -3,44 +3,46 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options 
 import os
+import time
 
-def startBot(username, password, url):
-	path = "/usr/local/bin/chromedriver.exe"
-	
-	# Create a Service object
-	service = Service(path)
+def startBot(username, password, url):	
+	# Create a Service object, WebDriver instance and Chrome Options
+	service = Service()
+	chrome_options = webdriver.ChromeOptions()
+	driver = webdriver.Chrome(service=service, options=chrome_options)
+	chrome_options.add_argument("--start-maximized") # [Optional] Launchs a maximized webpage
 
-	# Setup Chrome option (optional)
-	chrome_options = Options()
-	chrome_options.add_argument("--start-maximized") # Launchs a maximized webpage
-
-	# Initialize the Chrome browser with Serice and Options
-
-	# Create a WebDriver instance and giving the path of chromedriver to the selenium webdriver
-	driver = webdriver.Chrome(path)
-	
 	# Perform actions on the browser using WebDriver methods get which opens the website in chrome.
 	driver.get(url)
 	
 	# Find the id or name or class of
-	# username by inspecting on username input
-	driver.find_element_by_name("id/class/name of username").send_keys(username)
-	
-	# Find the password by inspecting on password input
-	driver.find_element_by_name("id/class/name of password").send_keys(password)
-	
-	# Click on submit
-	driver.find_element_by_css_selector("id/class/name/css selector of login button").click()
+	# username by inspecting on username input. The example below :
+	# driver.find_element_by_name("id/class/name of username").send_keys(username)
+	print("Filling in username field...")
+	driver.find_element(By.ID,"login_field").send_keys(username)
 
+	# Find the password by inspecting on password input. . Example below:
+	#driver.find_element_by_name("id/class/name of password").send_keys(password)
+	print("Filling in password field...")
+	driver.find_element(By.ID,"password").send_keys(password)
+
+	# Click on submit/login button. Example below:
+	# driver.find_element_by_css_selector("id/class/name/css selector of login button").click()
+	driver.find_element(By.NAME,"commit").click()
+	
+	print("Login started...")
+	time.sleep(20) # sleep for 5 seconds
+	
+	driver.quit()
 
 # Driver Code
-# Enter below your login credentials
+# Enter your login credentials below:
 username = "Enter your username"
 password = "Enter your password"
 
 # URL of the login page of site
 # where you want to automate login.
-url = "url"
+url = "https://github.com/login"
 
 # Call the function
-startBot(username, password, url)
+startBot(username,password,url)
